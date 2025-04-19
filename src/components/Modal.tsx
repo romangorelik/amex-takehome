091701203a061b9456e915565bfec4195040615b
Roman Gorelik <romangorelik@Mac.attlocal.net>
2025-04-19 13:48:43 -0400

    Add skeleton for components, install testing suites

---
+++ src/components/Modal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Modal = () => {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+export default Modal;
---
+++ src/components/Scenario.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Scenario = () => {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+export default Scenario;
