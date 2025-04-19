742b0b17c356375da47dedfdf920a6d117bea671
Roman Gorelik <romangorelik@Mac.attlocal.net>
2025-04-19 15:37:16 -0400

    Add comments to Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 
+/**
+ * @param onClose - function with no return that will close the modal
+ * @param header - string that will serve as the Modal's heading
+ */
 interface IModalProps {
   onClose: () => void;
   header?: string;
@@ -8,9 +12,9 @@ interface IModalProps {
 /**
  * Modal component
  * Renders modal with an onClose button that can also be triggered by the escape key
- *
  */
 const Modal = ({ onClose, header = "Modal Header" }: IModalProps) => {
+  // Create a sideeffect after rendering that will attach the escape key to the onClose function
   useEffect(() => {
     const close = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
@@ -19,9 +23,13 @@ const Modal = ({ onClose, header = "Modal Header" }: IModalProps) => {
     };
 
     window.addEventListener("keydown", close);
+
+    // Detach event listener when component is unmounted
     return () => window.removeEventListener("keydown", close);
   }, [onClose]);
 
+  // Basic Modal structure with a container, a heading, and a close button. The container has an onClick which stops propogation due to how we created the 
+  // onClick event on the parent container to close the modal when clicking outside.
   return (
     <div role="dialog" onClick={(e) => e.stopPropagation()} className="modal-container">
       <div role="heading">{header}</div>
